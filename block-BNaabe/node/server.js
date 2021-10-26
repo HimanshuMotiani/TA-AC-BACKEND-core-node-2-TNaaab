8c85f2be8a75071a51064810733f3d0913672bd1
HimanshuMotiani <himanshu.motiani93@gmail.com>
2021-10-26 12:25:05 +0530

    changes

--- block-BNaabe/node/server.js
+++ block-BNaabe/node/server.js
@@ -1,31 +1,32 @@
 console.log(__filename);
+console.log(__dirname + "/app.js");
+console.log("./index.html");
 let path = require("path");
 let http = require("http");
 let qs = require("querystring");
 console.log(path.join(__dirname, "index.html"));
 var server = http.createServer(handleRequest)
 
-function handleRequest(req,res){
+function handleRequest(req, res) {
     var dataFormat = req.headers['content-type'];
     var store = '';
- req.on("data",(chunk)=>{
-     store += chunk
- })
- if(dataFormat == "application/json"){
- req.on("end",()=>{
-     res.statusCode = 200;
-     res.end(store);
- })
-}
-if(dataFormat == "application/x-www-form-urlencoded") {
-    req.on("end",()=>{
-        let parsedData = qs.parse(store)
-        res.end(parsedData.captain);
+    req.on("data", (chunk) => {
+        store += chunk
+    })
+
+    req.on("end", () => {
+        if (dataFormat == "application/json") {
+            res.statusCode = 200;
+            res.end(store);
+        }
+        if (dataFormat == "application/x-www-form-urlencoded") {
+            let parsedData = qs.parse(store)
+            res.end(parsedData.captain);
+        }
     })
 }
-}
 
-server.listen(1111,()=>{})
+server.listen(1111, () => { })
 
 function handleRequest(req, res) {
     var dataFormat = req.headers['content-type'];
@@ -33,18 +34,18 @@ function handleRequest(req, res) {
     req.on("data", (chunk) => {
         store += chunk
     })
-    if (dataFormat == "application/json") {
-        req.on("end", () => {
+
+    req.on("end", () => {
+        if (dataFormat == "application/json") {
             res.end(store);
-        })
-    }
-    if (dataFormat == "application/x-www-form-urlencoded") {
-        req.on("end", () => {
+        }
+        if (dataFormat == "application/x-www-form-urlencoded") {
             let parsedData = qs.parse(store)
             res.end(JSON.stringify(parsedData));
-        })
-    }
+        }
+    })
 }
+
 
 function handleRequest(req, res) {
     var dataFormat = req.headers['content-type'];
@@ -52,18 +53,19 @@ function handleRequest(req, res) {
     req.on("data", (chunk) => {
         store += chunk
     })
-    if (dataFormat == "application/json") {
-        req.on("end", () => {
+    req.on("end", () => {
+        if (dataFormat == "application/json") {
             res.setHeader(`content-Type`, `text/html`)
-            res.end(`<h1>${store.name}</h1><h2>${store.email}</h2>`);
-        })
-    }
-    if (dataFormat == "application/x-www-form-urlencoded") {
-                req.on("end", () => {
-                    let parsedData = JSON.stringify(qs.parse(store))
-                    res.end(`<h2>${parsedData.email}</h2>`);
-                })
-    }
+            var jsonData = JSON.parse(store)
+            res.end(`<h1>${jsonData.name}</h1><h2>${jsonData.email}</h2>`);
+        }
+        if (dataFormat == "application/x-www-form-urlencoded") {
+            res.setHeader(`Content-Type`, `text/html`)
+            let formData = qs.parse(store);
+            res.end(`<h2>${formData.email}</h2>`);
+        }
+    })
+
 }
 
-server.listen(9000, () => { })
+ server.listen(9000, () => { })
